60895389e002b969d7dd90503f11695494f15834
7418293299 <144542981+7418293299@users.noreply.github.com>
2023-09-19 12:55:13 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-def factorial(n):
-  if n==0 or n==1:
-     return 1
-  else:
-      return n*factorial(n-1) 
-number=int(input("enter the number"))
-fact=factorial(number) 
-print(" THE FACTORIAL OF",number, "IS",fact)
+class BankAccount:
+  def __init__ (self,__accountNumber,__accountHolderName,__initialBalance=0):
+    self.__acccountNumber=__accountNumber
+    self.__accountHolder=__accountHolderName
+    self.__AccountBalance=__initialBalance
+    
+  def depositMoney(self,amount):
+    self.__AccountBalance +=amount
+    print("Rs.",amount," has been deposited in your account.")
+    
+  def withdrawMoney(self,amount):
+   if amount>self.__AccountBalance:
+     print("insufficient Balance")
+   else:
+    self.__AccountBalance -=amount
+    print("Rs.",amount," has beenwithdrawn from your account")
 
+  def checkBalance(self):
+    print("current balance: ",self.__AccountBalance)
 
-   
+obj=BankAccount(2838203938,"karthika")
+obj.depositMoney(5000)
+obj.withdrawMoney(2000)
+obj.checkBalance()
